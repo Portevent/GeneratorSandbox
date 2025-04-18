5058a17254b32d62579a2433c626feea8347c79d
Portevent <74187611+Portevent@users.noreply.github.com>
2025-04-18 17:13:16 +0200

    feat : ImagePalette from dir

--- color/image_palette_helper.py
+++ color/image_palette_helper.py
@@ -8,3 +8,7 @@ class ImagePaletteHelper:
     @staticmethod
     def FromFiles(files: List[str]) -> ImagePalette:
         return palette([ImageLoader.from_file(file) for file in files])
+
+    @staticmethod
+    def FromDir(dir: str) -> ImagePalette:
+        return ImagePaletteHelper.FromFiles([file for file in Path(dir).iterdir() if file.is_file()])
